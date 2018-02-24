8aae9bfbed9305f00e662d92b183aec5eecc66a0
srivastavaaviral1 <srivastavaaviral1@gmail.com>
2018-02-24 12:10:54 +0530

    adding the bubbles and popping them

--- js/skill_bubble.js
+++ js/skill_bubble.js
@@ -1,17 +1,21 @@
-var $body = $('.gen');
+$('.skill').click(function(){
+  $('.gen.hide').addClass('hidden');
+  $('.skill-section').removeClass('hidden');
 
-$('html').on('keydown', function (e) {
-  var $bubble;
-  
-  if(e.key == ' ') {
-    console.log($body[0].clientHeight);
-    $('.main').addClass('hidden');
-    $bubble = $('<div>');
-    $bubble.addClass('bubble');
-    $bubble.css({
+var $body = $('.skill-section.gen');
+  var $bubble = $('<div>');
+  $bubble.addClass('bubble');
+  $bubble.css({
       'top':  Math.random() * ($body[0].clientHeight - 100),
       'left': Math.random() * ($body[0].clientWidth - 100)
     });
-    $('.skill').append($bubble);
-  }
-});
+    $('.skill-section').append($bubble);
+
+ $body.on('click','.bubble', function(){
+      $(this).addClass('is-popping');
+    });
+    
+  $body.on('transitionend','.bubble', function(){
+    $(this).remove();
+  });
+  })
