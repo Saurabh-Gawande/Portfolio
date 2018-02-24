fa997a61e4e84b8294a371946ff2ae0cc294376b
srivastavaaviral1 <srivastavaaviral1@gmail.com>
2018-02-24 20:23:36 +0530

    adding skill chart

---
+++ js/skill_chart.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+   
+         $('.chart').click(function() {
+        $('.gen.hide').addClass('hidden');
+        $('.skill-chart').removeClass('hidden');
+        
+    });
+    
+  });
+
+  
